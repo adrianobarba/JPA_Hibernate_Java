92c47a7f8bb7563652fa3accc02fef3aea8e8cac
adriano barbosa <adrianobarbosa_4@hotmail.com>
2024-01-19 14:48:42 -0300

    commit jpa

--- src/main/java/br/com/adriano/testes/CadastroDeProduto.java
+++ src/main/java/br/com/adriano/testes/CadastroDeProduto.java
@@ -1,6 +1,8 @@
 package br.com.adriano.testes;
 
+import br.com.adriano.loja.dao.CategoriaDao;
 import br.com.adriano.loja.dao.ProdutoDao;
+import br.com.adriano.loja.modelo.Categoria;
 import br.com.adriano.loja.modelo.Produto;
 import br.com.adriano.loja.util.JPAUtil;
 
@@ -12,16 +14,20 @@ import java.math.BigDecimal;
 public class CadastroDeProduto {
 
     public static void main(String[] args) {
-        Produto celular = new Produto();
-        celular.setNome("Xiaomi Redmi");
-        celular.setDescricao("Muito legal");
-        celular.setPreco(new BigDecimal("800"));
+        Categoria celulares = new Categoria("CELULARES");
+        Produto celular = new Produto("Xiaomi Redmi", "Muito legal", new BigDecimal("800"), celulares);
+
+
 
         EntityManager em = JPAUtil.getEntityManager();
-        ProdutoDao dao = new ProdutoDao(em);
+        ProdutoDao produtoDao = new ProdutoDao(em);
+        CategoriaDao categoriaDao = new CategoriaDao(em);
 
         em.getTransaction().begin();
+
+        categoriaDao.cadastrar(celulares);
         em.persist(celular);
+
         em.getTransaction().commit();
         em.close();
 
